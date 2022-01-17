1644866269d45f58435189f74a71ed9c2813bbe5
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2022-01-17 10:52:41 +0100

    Improve css for frontend editing Add icon search mapping with bootstrap icons

--- includes/style.php
+++ includes/style.php
@@ -4,29 +4,29 @@ defined('_JEXEC') or die;
 
 $wa->addInlineStyle('
 :root {
-	--headerbg: ' . $params->get('headerbg') . ';
-	--headercolor: ' . $params->get('headercolor') . ';
+	--bannercolor: ' . $params->get('bannercolor') . ';
+	--bannerheight: ' . $params->get('bannerheight') . 'vh;
+	--banneroverlay: ' . $params->get('banneroverlay') . ';
 	--bodybg: ' . $params->get('bodybg') . ';
 	--bodycolor: ' . $params->get('bodycolor') . ';
-	--linkcolor: ' . $params->get('linkcolor') . ';
-	--linkcolorh: ' . $params->get('linkcolorh') . ';
-	--bannercolor: ' . $params->get('bannercolor') . ';
-	--banneroverlay: ' . $params->get('banneroverlay') . ';
+	--bodysize: ' . $params->get('bodysize') . 'rem;
+	--bottomabg:  ' . $params->get('bottomabg') . ';
+	--bottombbg:  ' . $params->get('bottombbg') . ';
 	--btnbg: ' . $params->get('btnbg') . ';
 	--btnbgh: ' . $params->get('btnbgh') . ';
 	--btncolor: ' . $params->get('btncolor') . ';
 	--btncolorh: ' . $params->get('btncolorh') . ';
 	--footerbg: ' . $params->get('footerbg') . ';
 	--footercolor: ' . $params->get('footercolor') . ';
-	--wrapperwidth: ' . $params->get('wrapperwidth') . 'px;
-	--bannerheight: ' . $params->get('bannerheight') . 'vh;
-	--bottomabg:  ' . $params->get('bottomabg') . ';
-	--bottombbg:  ' . $params->get('bottombbg') . ';
-	--topabg:  ' . $params->get('topabg') . ';
-	--topbbg:  ' . $params->get('topbbg') . ';
-	--bodysize: ' . $params->get('bodysize') . 'rem;
 	--h1size: ' . $params->get('h1') . 'rem;
 	--h2size: ' . $params->get('h2') . 'rem;
 	--h3size: ' . $params->get('h3') . 'rem;
+	--headerbg: ' . $params->get('headerbg') . ';
+	--headercolor: ' . $params->get('headercolor') . ';
+	--linkcolor: ' . $params->get('linkcolor') . ';
+	--linkcolorh: ' . $params->get('linkcolorh') . ';
+	--topabg:  ' . $params->get('topabg') . ';
+	--topbbg:  ' . $params->get('topbbg') . ';
+	--wrapperwidth: ' . $params->get('wrapperwidth') . 'px;
 }'
 );
