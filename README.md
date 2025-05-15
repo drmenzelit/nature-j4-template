8178129bdbfe839d5e1d6d98c0a85e2f17444f1d
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2025-05-15 16:46:21 +0200

    Version 1.1.6

--- README.md
+++ README.md
@@ -1,8 +1,5 @@
-# Nature - A Joomla 4 Template
-Nature is a clean, core-based, css-only Joomla 4 site template.
+# Nature - A Joomla Template
+Nature is a clean, core-based, css-only Joomla 4 and 5 site template.
 
 ## Demo
 [A demo site with explanations can be found here](https://j4.dr-menzel-it.de)
-
-## Version 1.1.0
-Nature is now child template capable
---
+++ html/mod_articles/timeline.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @package     Joomla.Site
+ * @subpackage  mod_articles
+ *
+ * @copyright   (C) 2024 Open Source Matters, Inc. <https://www.joomla.org>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\Layout\LayoutHelper;
+
+/** @var Joomla\CMS\WebAsset\WebAssetManager $wa */
+$wa = $app->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle('timeline', 'timeline.css');
+
+if (!$list)
+{
+    return;
+}
+
+?>
+<div id="timeline">
+    <?php for ($i = 0, $n = count($list); $i < $n; $i ++) : ?>
+    <?php $item = $list[$i]; ?>
+    <div class="timeline-item">
+        <div class="timeline-icon">
+        </div>
+        <div class="timeline-content">
+            <h2><?php echo $item->title; ?></h2>
+
+            <?php if (in_array($params->get('img_intro_full'), ['intro', 'full']) && !empty($item->imageSrc)) : ?>
+                <?php echo LayoutHelper::render('joomla.content.' . $params->get('img_intro_full') . '_image', $item); ?>
+            <?php endif; ?>
+
+            <?php echo $item->introtext; ?>
+        </div>
+    </div>
+    <?php endfor; ?>
+</div>
+
